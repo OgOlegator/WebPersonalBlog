a31bc7ef95243d827c3128fc0101cd7412894631
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2022-10-09 21:08:27 +0300

    Добавлены методы для API контроллера и тестовые данные в БД

--- Blog.Services.PostsAPI/Controllers/PostAPIController.cs
+++ Blog.Services.PostsAPI/Controllers/PostAPIController.cs
@@ -1,10 +1,112 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Services.PostsAPI.Models.Dto;
+using Blog.Services.PostsAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Services.PostsAPI.Controllers
 {
+    [ApiController]
     [Route("api/posts")]
     public class PostAPIController : ControllerBase
     {
-        
+        protected ResponseDto _response;
+        private IPostRepository _repository;
+
+        public PostAPIController(IPostRepository repository)
+        {
+            _response = new ResponseDto();
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                _response.Result = await _repository.GetPosts();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                _response.Result = await _repository.GetPostById(id);
+                if(_response.Result == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.DisplayMessage = "Пост не найден";
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                _response.Result = await _repository.DeletePost(id);
+                if((bool)_response.Result)
+                {
+                    _response.IsSuccess = false;
+                    _response.DisplayMessage = "Не удалось удалить запись";
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody] PostDto postDto)
+        {
+            try
+            {
+                _response.Result = await _repository.CreateUpdatePost(postDto);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public async Task<object> Put([FromBody] PostDto postDto)
+        {
+            try
+            {
+                _response.Result = await _repository.CreateUpdatePost(postDto);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
     }
 }
---
+++ Blog.Services.PostsAPI/Migrations/20221009180354_SendPosts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Blog.Services.PostsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SendPosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "CreatedDate", "Name", "Text" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2022, 10, 9, 21, 3, 54, 477, DateTimeKind.Local).AddTicks(255), "Тестовый пост 1", "..." },
+                    { 2, new DateTime(2022, 10, 9, 21, 3, 54, 477, DateTimeKind.Local).AddTicks(295), "Тестовый пост 2", "..." },
+                    { 3, new DateTime(2022, 10, 9, 21, 3, 54, 477, DateTimeKind.Local).AddTicks(305), "Тестовый пост 3", "..." },
+                    { 4, new DateTime(2022, 10, 9, 21, 3, 54, 477, DateTimeKind.Local).AddTicks(314), "Тестовый пост 4", "..." }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 4);
+        }
+    }
+}
