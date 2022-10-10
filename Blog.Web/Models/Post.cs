e698257c4185c2e3e3d55d3f957ee9615fcebf2d
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2022-10-11 00:07:13 +0300

    Изменен фронтенд

---
+++ Blog.Web/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Web.Models
+{
+    public class Post
+    {
+
+        public int PostId { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+
+        [Display(Name = "Create Date")]
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+
+
+        [StringLength(300, MinimumLength = 0)]
+        public string Text { get; set; }
+
+    }
+}
