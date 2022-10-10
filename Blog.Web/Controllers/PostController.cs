a0dbef16a57850813ebf71d7dea6be5ced6fb44b
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2022-10-11 00:07:13 +0300

    Изменен фронтенд

---
+++ Blog.Web/Controllers/PostController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: PostController
+        public ActionResult PostIndex()
+        {
+            return View();
+        }
+
+        // GET: PostController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+    }
+}
