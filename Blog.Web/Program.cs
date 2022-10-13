e33e90d0a2c780d608e3c92c6f32a412b83e2d1a
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2022-10-13 20:37:06 +0300

    Добавлен контроллер для страницы PostIndex Измнено представление PostIndex Устранена ошибка запуска веб-приложени

--- Blog.Web/Program.cs
+++ Blog.Web/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Post}/{action=PostIndex}/{id?}");
 
 app.Run();
