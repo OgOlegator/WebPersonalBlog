7f072567712ddd99ca6091ad705cc7006ec2ae9c
OgOlegator <92753056+OgOlegator@users.noreply.github.com>
2023-03-31 16:55:12 +0300

    Добавлено удаление роли на странице Админа

--- Blog.Services.Identity/Controllers/RoleAdminController.cs
+++ Blog.Services.Identity/Controllers/RoleAdminController.cs
@@ -114,5 +114,61 @@ namespace Blog.Services.Identity.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string roleId)
+        {
+            var role = _roleManager.Roles.FirstOrDefault(x => x.Id == roleId);
+
+            if (role == null)
+                return NotFound();
+
+            var usersInRole = await _userManager.GetUsersInRoleAsync(role.Name);
+
+            var viewModel = new RoleDeleteViewModel
+            {
+                RoleId = role.Id,
+                RoleName = role.Name,
+                Members = usersInRole.Select(user => user.UserName),
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RoleDeleteViewModel viewModel)
+        {
+            bool isAllUsersDeleteInRole = true;
+
+            foreach (var user in await _userManager.GetUsersInRoleAsync(viewModel.RoleId))
+            {
+                var result = await _userManager.RemoveFromRoleAsync(user, viewModel.RoleId);
+
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError(String.Empty, $"Не удалось удалить роль у пользователя {user}");
+                    isAllUsersDeleteInRole = false;
+                }
+            }
+
+            if(!isAllUsersDeleteInRole)
+                return View(viewModel);
+
+            var role = _roleManager.Roles.FirstOrDefault(x => x.Id == viewModel.RoleId);
+
+            if(role == null)
+            {
+                ModelState.AddModelError(string.Empty, "Роль не найдена");
+                return View(viewModel);
+            }
+
+            var deleteRoleResult = await _roleManager.DeleteAsync(role);
+
+            if(deleteRoleResult.Succeeded)
+                return RedirectToAction(nameof(Index));
+
+            ModelState.AddModelError(string.Empty, $"Не удалось удалить роль - {viewModel.RoleName}");
+            return View(viewModel);
+        }
     }
 }
